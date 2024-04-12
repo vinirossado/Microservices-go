e7ce79ab2bcabba7f119c4910a4ce9898554f4ea
Vinicius Rossado <vinirossado@gmail.com>
2024-04-13 01:41:58 +0300

    Basic main startup

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -1,10 +1,35 @@
 package main
 
+import (
+	"authentication/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+)
+
 const webPort = "8080"
 
 type Config struct {
+	DB     *sql.DB
+	Models data.Models
 }
 
 func main() {
+
+	log.Println("Starting authentication service...")
+
+	// TODO: Connect to DB
+
+	app := Config{}
+	svr := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := svr.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
 
 }
--- authentication-service/go.mod
+++ authentication-service/go.mod
@@ -1,3 +1,10 @@
 module authentication
 
 go 1.22.2
+
+require golang.org/x/crypto v0.22.0
+
+require (
+	github.com/go-chi/chi/v5 v5.0.12 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+)
