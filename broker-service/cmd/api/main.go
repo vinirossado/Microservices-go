750cace68f6b0041ab6f17aad1b7ecf34bfd0e5e
Vinicius Rossado <vinirossado@gmail.com>
2024-04-12 22:33:39 +0300

    Basic broker microservice

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+const webPort = "8081"
+
+type Config struct{}
+
+func main() {
+	app := &Config{}
+
+	log.Printf("Starting broker service on port %s\n", webPort)
+
+	svr := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := svr.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
 }
